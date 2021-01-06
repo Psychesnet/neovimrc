069d3514cf79cd8f5cc0bc17add430233fd1bbd5
eric hsieh <psychesnet@gmail.com>
2021-01-06 14:27:58 +0800

    Add env variables

---
+++ .env/openwrt.sh
@@ -0,0 +1,20 @@
+export CCNAME=openwrt
+export USER=root
+export PASSWD=admin
+export IP=172.31.5.204
+export HTTP_PORT=80
+export HTTPS_PORT=443
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+export PS1="[\[\033[01;31m\]\$(echo \${CCNAME})\[\e[0m\]@\[\033[01;34m\]\$(echo \${IP})\[\e[0m\]] \W\$ "
+export GST_DEBUG="*:3"
+
+get() {
+  curl -k -u "${USER}:${PASSWD}" --anyauth -X GET "https://${IP}:${HTTPS_PORT}/adc-api/${1}"
+  echo
+}
+
+post() {
+  curl -k -u "${USER}:${PASSWD}" --anyauth -X POST -F upload=@${2} "https://${IP}:${HTTPS_PORT}/adc-api/${1}"
+  echo
+}
